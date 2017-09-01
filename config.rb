ed92689e53626d9254f35f97d4cfcb6ecb145608
Rey Padilla <padilla.reyj@gmail.com>
2017-09-01 18:01:12 +0700

    restructure

---
+++ config.rb
@@ -0,0 +1,13 @@
+require 'susy'
+require 'breakpoint'
+
+project_type = :stand_alone
+http_path = "/"
+sass_dir = "scss"
+css_dir = "css"
+images_dir = "images"
+javascript_dir = "js"
+line_comments = false
+preferred_syntax = :scss
+output_style = :compressed
+relative_assets = true
